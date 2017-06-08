4d29f4645d53ee988670be910724a8eec6ea754b
Goyo <gregorio.granja@gmail.com>
2017-06-08 12:11:14 +0200

    Añadir Modelo

---
+++ Ejemplo1/Ejemplo1/Models/Empleado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejemplo1.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public DateTime FechaDeAlta { get; set; }
+        public Sexo Sexo { get; set; }
+        public Boolean Activo  { get; set; }
+        public String Email { get; set; }
+    }
+    public enum Sexo {Masculino,Femenino }
+}
